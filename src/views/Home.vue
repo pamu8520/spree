0b512e49e1f865f9c8443d22ab7aee94afd824c4
Emil Engelin <32549182+MeisterOfPizza@users.noreply.github.com>
2022-02-03 10:52:01 +0100

    Setup branch (#1)
    * setup
    
    * startup version
    
    * Ignore node_modules
    
    * Lint
    
    * Info
    
    * Clean project
    
    * Remove .DS_Store
    
    * Add favicon to index.html
    
    Co-authored-by: ehak123 <73587185+ehak123@users.noreply.github.com>

---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <div
+            v-for="burger in burgers"
+            v-bind:key="burger.name"
+        >
+            <span>{{ burger.name }}</span>
+            <span>{{ burger.kCal }}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+    components: {
+    },
+    data() {
+        return {
+            burgers: [
+                { name: 'small burger', kCal: 250 },
+                { name: 'standard burger', kCal: 450 },
+                { name: 'large burger', kCal: 850 },
+            ],
+        };
+    },
+};
+</script>
